f14ca8c9b4c2327aa3ad71671a37d4b063fccf16
Mohamed Khaled <168598643+mohamed-khaledd@users.noreply.github.com>
2024-12-18 18:57:36 +0200

    Update register_file.v

--- register_file.v
+++ register_file.v
@@ -21,15 +21,10 @@ module register_file(RA1, RA2, WA, WE, WD, clk, RD1, RD2);
     reg [31:0] regFile [0:31];
     integer i;
 
-    initial begin
-        
+    initial begin        
         for (i = 0; i < 32; i = i + 1) begin
             regFile[i] = 32'b0;    // Initializing all registers to zero
         end
-
-        regFile[8] = 32'd10;   // $t0 initialized to 10
-        regFile[9] = 32'd20;   // $t1 initialized to 20
-        regFile[10] = 32'd30;  // $t2 initialized to 30
     end
 
     assign RD1 = regFile[RA1];
