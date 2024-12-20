13158f12157c18c86546c030515d18a7ad6698de
Mohamed Khaled <168598643+mohamed-khaledd@users.noreply.github.com>
2024-12-20 22:46:10 +0200

    Update instruction_memory.v

--- instruction_memory.v
+++ instruction_memory.v
@@ -1,13 +1,13 @@
-module instruction_memory(A, RD);
+module InstructionMemory(
+    input [31:0] addr,
+    output [31:0] data_out
+);
   
   /* Module Description:
-     - The module has an input A which is the address of the instruction to be fetched.
-     - The module has an output RD which is the instruction read.
+     - The module has an input addr which is the address of the instruction to be fetched.
+     - The module has an output data_out which is the instruction read.
      Note: A is 10 bits as the size of instruction memory is 4 KB.
   */
-  
-    input wire [9:0] A;
-    output wire [31:0] RD;
  
     reg [31:0] memory [0:1023];
     integer i;
@@ -17,6 +17,6 @@ module instruction_memory(A, RD);
             memory[i] = 32'b0;    // // Initializing all memory to zero
         end
     end
-    assign RD = memory[A];
+    assign data_out = memory[addr[31:2]];  // word addressing, Least significant 2 bits are zeros
 endmodule
 
