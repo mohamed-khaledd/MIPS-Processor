20f234c0aebb833dd3111c41c5003532cf5ce760
Mohamed Khaled <168598643+mohamed-khaledd@users.noreply.github.com>
2024-12-20 23:25:47 +0200

    Update control_unit.v

--- control_unit.v
+++ control_unit.v
@@ -1,107 +1,53 @@
-module control_unit(op,funct,MemtoReg,MemWrite,branch,alu_control,alu_src,RegDst,RegWrite,jump);
+module ControlUnit(
+    input [5:0] opcode, funct,
+    output reg mem_to_reg, mem_write, alu_src, branch, reg_dst, reg_write, jump,
+    output reg [2:0] alu_control
+);
 
   /* Module Description: 
    - The module has 2 inputs:   
-       1) op, which is the op-code of the instruction.
+       1) opcode, which is the op-code of the instruction.
        2) funct, which is the function of r-format instructions.
    - The module has 8 outputs:
-       1) MemtoReg, which decides whether the data to be written into a register comes from the memory or from the ALU result.
-       2) MemWrite, which enables writing data to memory.
+       1) mem_to_reg, which decides whether the data to be written into a register comes from the memory or from the ALU result.
+       2) mem_write, which enables writing data to memory.
        3) branch, which is the branch signal.
        4) alu_control, which is the alu control signal.
        5) alu_src, which determines the second operand for the ALU operation.
-       6) RegDst, which determines which register will be written to.
-       7) RegWrite, which enables writing data to a register.
+       6) reg_dst, which determines which register will be written to.
+       7) reg_write, which enables writing data to a register.
        8) jump, which is the jump signal.
   */
 
-  input wire [5:0] op,funct;
-  output reg MemtoReg,MemWrite,branch,alu_src,RegDst,RegWrite,jump;
-  output reg [2:0] alu_control;
+    always @(*) begin
+        mem_to_reg = 0; mem_write = 0; alu_src = 0; branch = 0; reg_dst = 0; reg_write = 0; jump = 0; alu_control = 3'b000;   // default values
 
-  always @ (*) begin
-      case(op)
-          6'b000000: // R-format
-           begin
-              case(funct)
-                 6'b100000: alu_control = 3'b010;  // add
-                 6'b100010: alu_control = 3'b110;  // subtract
-                 6'b100100: alu_control = 3'b000;  // and
-                 6'b100101: alu_control = 3'b001;  // or
-                 6'b101010: alu_control = 3'b111;  // slt
-              endcase
-              MemtoReg = 1'b0;
-              MemWrite = 1'b0;
-              branch = 1'b0;
-              alu_src = 1'b0;
-              RegDst = 1'b1;
-              RegWrite  = 1'b1;
-              jump = 1'b0;
+        case (opcode)
+            6'b000000: begin // R-type
+                reg_dst = 1; reg_write = 1;
+                case (funct)
+                    6'b100000: alu_control = 3'b010; // ADD
+                    6'b100010: alu_control = 3'b110; // SUB
+                    default: alu_control = 3'b000;
+                endcase
             end
-
-          6'b100011: // lw
-            begin
-              alu_control = 3'b010;
-              MemtoReg = 1'b1;
-              MemWrite = 1'b0;
-              branch = 1'b0;
-              alu_src = 1'b1;
-              RegDst = 1'b0;
-              RegWrite  = 1'b1;
-              jump = 1'b0;
+          
+            6'b100011: begin // LW
+                alu_src = 1; mem_to_reg = 1; reg_write = 1; alu_control = 3'b010;
             end
-
-
-          6'b101011: // sw
-            begin
-              alu_control = 3'b010;
-              MemtoReg = 1'bx;
-              MemWrite = 1'b1;
-              branch = 1'b0;
-              alu_src = 1'b1;
-              RegDst = 1'bx;
-              RegWrite  = 1'b0;
-              jump = 1'b0;
+          
+            6'b101011: begin // SW
+                alu_src = 1; mem_write = 1; alu_control = 3'b010;
             end
-
-          6'b000100: // beq
-            begin
-              alu_control = 3'b110;
-              MemtoReg = 1'bx;
-              MemWrite = 1'b0;
-              branch = 1'b1;
-              alu_src = 1'b0;
-              RegDst = 1'bx;
-              RegWrite  = 1'b0;
-              jump = 1'b0;
+          
+            6'b000100: begin // BEQ
+                branch = 1; alu_control = 3'b110;
             end
-
-          6'b001000: // addi
-            begin 
-              alu_control = 3'b010;  
-              MemtoReg = 1'b0;
-              MemWrite = 1'b0;
-              branch = 1'b0;
-              alu_src = 1'b1;
-              RegDst = 1'b0;
-              RegWrite  = 1'b1;
-              jump = 1'b0;
+          
+            6'b000010: begin // JUMP
+                jump = 1;
             end
-
-
-          6'b000010: // j 
-            begin
-              alu_control = 3'bxxx;   
-              MemtoReg = 1'bx;
-              MemWrite = 1'b0;
-              branch = 1'bx;
-              alu_src = 1'bx;
-              RegDst = 1'bx;
-              RegWrite  = 1'b0;
-              jump = 1'b1;
-            end
-
-      endcase
+            default: ;
+        endcase
     end
-
 endmodule
